04a5c4351b15ed0a9b0120312a96fe1b94c152ec
Mehran Mousavi <mehran.mousavi@outlook.com>
2024-10-14 08:54:51 +0330

    init

---
+++ _next/static/chunks/app/layout-e07e40e2ea12c601.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7019:function(n,e,u){Promise.resolve().then(u.t.bind(u,3527,23))},3527:function(){}},function(n){n.O(0,[613,19,892,744],function(){return n(n.s=7019)}),_N_E=n.O()}]);
