234b22ddb50247ffcea0b6698fcd1347c04ad236
Mehran Mousavi <mehran.mousavi@outlook.com>
2024-10-29 07:33:46 +0330

    release 1

--- _next/static/chunks/app/layout-35bf9fa2162a4cbb.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{2547:function(n,e,t){Promise.resolve().then(t.t.bind(t,2835,23)),Promise.resolve().then(t.t.bind(t,237,23))},237:function(){},2835:function(){}},function(n){n.O(0,[940,355,928,744],function(){return n(n.s=2547)}),_N_E=n.O()}]);
